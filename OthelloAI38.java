2b2fa0b03dc7335f3a05431be9306e9724408fd4
Magnus <magnus@pop-os.localdomain>
2022-02-28 13:59:14 +0100

    update minimax / remove alpha beta

--- OthelloAI38.java
+++ OthelloAI38.java
@@ -1,47 +1,111 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class OthelloAI38 implements IOthelloAI {
-    private Position aiMove;
+    private class Move {
+        Position position;
+        int value;
+
+        Move(Position position, int value) {
+            this.position = position;
+            this.value = value;
+        }
+    }
 
     @Override
     public Position decideMove(GameState s) {
         boolean isMaximizingPlayer = s.getPlayerInTurn() == 2;
-        aiMove = new Position(-1, -1);
-        minimaxAlphaBeta(s, 6, Integer.MIN_VALUE, Integer.MAX_VALUE, isMaximizingPlayer);
-        return aiMove;
+
+        System.out.println("Possible moves:");
+        for (Position p : s.legalMoves()) {
+            System.out.println(p);
+        }
+        System.out.println("--------------------");
+
+        Move move = minimax(s, 6, isMaximizingPlayer, new Position(-1, -1));
+        System.out.println("OthelloAI38: " + move.position + " Value: " + move.value);
+        return move.position;
     }
 
-    private int minimaxAlphaBeta(GameState s, int depth, int alpha, int beta, boolean maximizingPlayer) {
+    private Move minimax(GameState s, int depth, boolean maximizingPlayer, Position position) {
         if (depth == 0 || s.isFinished()) {
-            return maximizingPlayer ? s.countTokens()[1] - s.countTokens()[0] : s.countTokens()[0] - s.countTokens()[1];
+            int value = getMaximizingPlayerScore(s);
+
+            if (isCorner(s, position)) {
+                value += maximizingPlayer ? -5 : 5;
+            }
+
+            System.out.println(position + " | value: " + value);
+            return new Move(position, value);
         }
 
         if (maximizingPlayer) {
-            int maxEval = Integer.MIN_VALUE;
-            for (Position p : s.legalMoves()) {
-                GameState newState = new GameState(s.getBoard(), s.getPlayerInTurn());
-                newState.insertToken(p);
-                int eval = minimaxAlphaBeta(newState, depth - 1, alpha, beta, false);
-                maxEval = Math.max(maxEval, eval);
-                aiMove = p;
-                alpha = Math.max(alpha, eval);
-                if (beta <= alpha) {
-                    break;
+            Move maxMove = new Move(new Position(-1, -1), Integer.MIN_VALUE);
+
+            if (s.legalMoves().isEmpty()) {
+                GameState evalState = copyState(s);
+                evalState.changePlayer();
+                Move eval = minimax(evalState, depth - 1, false, position);
+
+                if (eval.value > maxMove.value) {
+                    maxMove = new Move(position, eval.value);
+                }
+
+            } else {
+                for (Position p : s.legalMoves()) {
+                    GameState evalState = copyState(s);
+                    evalState.insertToken(p);
+                    Move eval = minimax(evalState, depth - 1, false, p);
+                    int a = evalState.countTokens()[0];
+                    int b = evalState.countTokens()[1];
+                    if (eval.value > maxMove.value) {
+                        maxMove = new Move(p, eval.value);
+                    }
                 }
             }
-            return maxEval;
+            return maxMove;
         } else {
-            int minEval = Integer.MAX_VALUE;
-            for (Position p : s.legalMoves()) {
-                GameState newState = new GameState(s.getBoard(), s.getPlayerInTurn());
-                newState.insertToken(p);
-                int eval = minimaxAlphaBeta(newState, depth - 1, alpha, beta, true);
-                minEval = Math.min(minEval, eval);
-                aiMove = p;
-                beta = Math.min(beta, eval);
-                if (beta <= alpha) {
-                    break;
+            Move minMove = new Move(new Position(-1, -1), Integer.MAX_VALUE);
+
+            if (s.legalMoves().isEmpty()) {
+                GameState evalState = copyState(s);
+                evalState.changePlayer();
+                Move eval = minimax(evalState, depth - 1, false, position);
+
+                if (eval.value > minMove.value) {
+                    minMove = new Move(position, eval.value);
                 }
             }
-            return minEval;
+            for (Position p : s.legalMoves()) {
+                GameState evalState = copyState(s);
+                evalState.insertToken(p);
+                Move eval = minimax(evalState, depth - 1, true, p);
+
+                if (eval.value < minMove.value) {
+                    minMove = new Move(p, eval.value);
+                }
+            }
+            return minMove;
         }
+    }
+
+    private GameState copyState(GameState s) {
+        return new GameState(s.getBoard(), s.getPlayerInTurn());
+    }
+
+    private boolean isCorner(GameState s, Position p) {
+        int maxIndex = s.getBoard().length-1;
+        List<Position> corners = new ArrayList<>(
+                List.of(
+                        new Position(0,0),
+                        new Position(0,maxIndex),
+                        new Position(maxIndex, 0),
+                        new Position(maxIndex, maxIndex)
+                ));
+        return corners.contains(p);
+    }
+
+    private int getMaximizingPlayerScore(GameState s) {
+        return s.countTokens()[1] - s.countTokens()[0];
     }
 }
